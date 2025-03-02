c12dd89f94164f21792a0a26bb2fe0c4d571df43
ITSAIDI <noureddinesaidi111@gmail.com>
2025-03-02 16:40:27 +0000

    c

--- applewebsite/app/(root)/page.jsx
+++ applewebsite/app/(root)/page.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
+import assets from '../../utils/utilitis';
+
 
 export default function rootPage() {
   return (
-    <div>rootPage</div>
+    <div>rootPage
+      
+    </div>
   )
 }
