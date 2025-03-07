4dec4cc8543024edad120d56022d3e53b669b0b1
ITSAIDI <noureddinesaidi111@gmail.com>
2025-03-07 16:04:31 +0000

    c

--- applewebsite/components/Hero.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Hero() {
-  return (
-    <div>
-      <h1 className='text-white'>Hero</h1>
-    </div>
-  )
-}
--- applewebsite/components/Highlights.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Highlights() {
-  return (
-    <div>
-      <h1 className='text-white'>Highlights</h1>
-    </div>
-  )
-}
